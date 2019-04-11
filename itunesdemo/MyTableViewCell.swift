265dea966860bf8a6559623eb38a248c94a739c4
Bemisal <bemisalsarfraz@gmail.com>
2019-04-12 01:16:19 +0500

    setup all design work programatically

---
+++ itunesdemo/MyTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  MyTableViewCell.swift
+//  itunesdemo
+//
+//  Created by Yousaf on 11/04/2019.
+//  Copyright © 2019 Codenterprise. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    lazy var backview:UIView = {
+        let view  = UIView(frame: CGRect(x: 10, y: 6, width: self.frame.width-20, height: 110))
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    
+    lazy var myimage : UIImageView = {
+        let image = UIImageView (frame: CGRect(x: 4, y: 4, width: 104, height: 104))
+        image.contentMode = .scaleAspectFill
+        image.layer.cornerRadius = 52
+        image.clipsToBounds = true
+        return image
+    }()
+    
+    lazy var namelbl : UILabel = {
+        let namelbl = UILabel (frame: CGRect(x: 116, y: 8, width: backview.frame.width - 116, height: 30))
+        namelbl.textAlignment = .left
+        namelbl.font = UIFont.boldSystemFont(ofSize: 18)
+        return namelbl
+    }()
+    
+    lazy var mediatypelbl : UILabel = {
+        let mediatypelbl = UILabel (frame: CGRect(x: 116, y: 42, width: backview.frame.width - 116, height: 30))
+        mediatypelbl.textAlignment = .left
+        mediatypelbl.font = UIFont.boldSystemFont(ofSize: 18)
+        return mediatypelbl
+    }()
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        addSubview(backview)
+        backview.addSubview(myimage)
+        backview.addSubview(namelbl)
+        backview.addSubview(mediatypelbl)
+    }
+    override func layoutSubviews() {
+        contentView.backgroundColor = UIColor.clear
+        backgroundColor = UIColor.clear
+        backview.layer.cornerRadius = 5
+        backview.clipsToBounds = true
+    }
+
+}
--- itunesdemo/ViewController.swift
+++ itunesdemo/ViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+
+    var maintableview : UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        //setting maintableview
+      //  maintableview.frame = self.view.frame
+        let barheight : CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displaywidth: CGFloat = self.view.frame.width
+        let displayheight : CGFloat = self.view.frame.height
+        maintableview = UITableView(frame: CGRect(x: 0, y: 0, width: displaywidth, height: displayheight - barheight))
+        maintableview.separatorColor = UIColor.clear
+        maintableview.backgroundColor = UIColor.gray
+        maintableview.dataSource=self
+        maintableview.delegate=self
+         self.view.addSubview(maintableview)
+        maintableview.register(MyTableViewCell.self, forCellReuseIdentifier: "Cell")
 
+       
+    }
+    //uitableview datasource methods
+    // func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //  }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? MyTableViewCell else {fatalError("error in cell creation")}
+        cell.myimage.image = UIImage(named: "noimage.jpeg")
+        cell.namelbl.text = "Pink"
+        cell.mediatypelbl.text = "Audio"
+        return cell
+    }
+    //uitableview delegate method
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
+    {
+       // return 118
+        return 122
+    }
 
 }
 
